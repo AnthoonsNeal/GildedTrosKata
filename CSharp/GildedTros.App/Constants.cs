9faee6a529ac1f243d3021a88d27e1796c6cc9e0
neal.anthoons <neal.anthoons@agidens.com>
2024-01-23 15:58:06 +0100

    ItemFactory to have a cleaner way of constructing an item

---
+++ CSharp/GildedTros.App/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace GildedTros.App;
+
+public static class Constants
+{
+    public static string RING_OF_CLEANSENING_CODE = "Ring of Cleansening Code";
+    public static string GOOD_WINE = "Good Wine";
+    public static string ELIXIR_OF_THE_SOLID = "Elixir of the SOLID";
+    public static string B_DAWG_KEYCHAIN = "B-DAWG Keychain";
+    public static string BACKSTAGE_PASSES_REFACTOR = "Backstage passes for Re:factor";
+    public static string BACKSTAGE_PASSES_HAXX = "Backstage passes for HAXX";
+    public static string DUPLICATE_CODE = "Duplicate Code";
+    public static string LONG_METHODS = "Long Methods";
+    public static string UGLY_VARIABLE_NAMES = "Ugly Variable Names";
+}
---
+++ CSharp/GildedTros.App/ItemFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GildedTros.App;
+
+public static class ItemFactory
+{
+    public static Item CreateRingOfCleansening(int sellIn, int quality)
+        => CreateItem(Constants.RING_OF_CLEANSENING_CODE, sellIn, quality);
+
+    public static Item CreateGoodWine(int sellIn, int quality)
+        => CreateItem(Constants.GOOD_WINE, sellIn, quality);
+
+    public static Item CreateElixirOfTheSolid(int sellIn, int quality)
+        => CreateItem(Constants.ELIXIR_OF_THE_SOLID, sellIn, quality);
+
+    public static Item CreateBDawgKeyChain(int sellIn, int quality)
+        => CreateItem(Constants.B_DAWG_KEYCHAIN, sellIn, quality);
+
+    public static Item CreateBackstagePassesForRefactor(int sellIn, int quality)
+        => CreateItem(Constants.BACKSTAGE_PASSES_REFACTOR, sellIn, quality);
+
+    public static Item CreateBackstagePassesForHaxx(int sellIn, int quality)
+        => CreateItem(Constants.BACKSTAGE_PASSES_HAXX, sellIn, quality);
+
+    public static Item CreateDuplicateCode(int sellIn, int quality)
+        => CreateItem(Constants.DUPLICATE_CODE, sellIn, quality);
+    
+    public static Item CreateLongMethods(int sellIn, int quality)
+        => CreateItem(Constants.LONG_METHODS, sellIn, quality);
+    
+    public static Item CreateUglyVariableNames(int sellIn, int quality)
+        => CreateItem(Constants.UGLY_VARIABLE_NAMES, sellIn, quality);
+    
+    private static Item CreateItem(string name, int sellIn, int quality)
+        => new()
+        {
+            Name = name,
+            SellIn = sellIn,
+            Quality = quality
+        };
+}
